735d63e2c16dc8726cc4602486e9a54a4e58c9da
Rajiv Dulepet <rajivdulepet@Da-Vinci.attlocal.net>
2022-03-14 10:40:05 -0700

    added american variations and adult/child search maps

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,14 +16,18 @@ def get_search_term_mapping():
 
 st.title('Specialist Doctor Search')
 search_space = get_search_term_mapping()
-search_terms = []
-search_terms = st.multiselect(
-    "Select search terms", list(search_space.keys()), []
+#search_terms = []
+search_term = st.selectbox(
+    "Select search terms", list(search_space.keys())
 )
 
 addln_suggestions = []
-for term in search_terms:
-    addln_suggestions.extend(search_space[term])
+#for term in search_terms:
+addln_suggestions.extend(search_space[search_term])
+
+# only unique terms
+addln_suggestions = list(set(addln_suggestions))
+addln_suggestions = [term for term in addln_suggestions if term != search_term]
 
 addln_search_terms = []
 if len(addln_suggestions) > 0:
@@ -31,8 +35,7 @@ if len(addln_suggestions) > 0:
         "Optionally add related terms", addln_suggestions, []
     )
 
-if len(search_terms) > 0:
-    st.write(f'User selected search terms: {search_terms}')
+st.write(f'User selected search terms: {search_term}')
 if len(addln_search_terms) > 0:
     st.write(f'User selected additional related terms: {addln_search_terms}')
 
